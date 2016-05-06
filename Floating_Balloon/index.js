dcd1d6e5f19058a726d3efef108c8709d392fe31
Daisy Tucker <daisyatucker@gmail.com>
2016-05-06 16:27:05 +0100

    Improved balloon movement

--- Floating_Balloon/index.js
+++ Floating_Balloon/index.js
@@ -2,72 +2,78 @@ $(init);
 
 function init() {
 
-    $("body").keydown(function(e) {
-        switch (e.which) {
-        case 37:
-            $('#balloon').animate({
-                left: '-=100px'
-            }); //left
-            break;
-        case 38:
-            $('#balloon').animate({
-                top: '-=5px'
-            }); //up
-            break;
-        case 39:
-            $('#balloon').animate({
-                left: '+=100px'
-            }); //right
-            break;
-        case 40:
-            $('#balloon').animate({
-                top: '+=5px'
-            }); //bottom
-            break;
-        }
-    })
-
     // Up: 38
     // Down: 40
     // Right: 39
     // Left: 37
 
 
-    $(document).keydown(function(e) {
-        console.log(e.keyCode);
+        $(function(){
+
+        var playerMoving = false;
+        var playerLeft = false;
+        var playerUp = false;
+        var playerRight = false;
+        var playerDown = false;
+
+          $(document).keydown(function(e) {
+
+            if (e.keyCode === 37){
+                playerLeft = true;
+            } else if (e.keyCode === 38) {
+                playerUp = true;
+            } else if(e.keyCode === 39) {
+                playerRight = true;
+            } else if (e.keyCode === 40) {
+                playerDown = true;
+            }   
+
+          });
+
+          $(document).keyup(function(e) {
+
+            if (e.keyCode === 37) {
+                playerLeft = false;
+            } else if (e.keyCode === 38) {
+                playerUp = false;
+            } else if(e.keyCode === 39) {
+                playerRight = false;
+            } else if (e.keyCode === 40) {
+                playerDown = false;
+            }   
+          })
+
+          $(document).keydown(function(e) {
+              console.log(e.keyCode);
+
+              // move the balloon by 1 pixel each frame
+              // change this to make it move faster
+              if(playerLeft) {
+              $("#balloon").css({"left" : "-=1px"});
+            } if (playerUp) {
+              $("#balloon").css({"top" : "-=1px"});
+            } if (playerRight) {
+              $("#balloon").css({"left" : "+=1px"});
+            } if (playerDown) {
+              $("#balloon").css({"top" : "+=1px"});
+            }
+          });
+
+          // create the animation loop every 5ms
+          // this sets the frame rate of your game
+          var animateInterval = setInterval(animate, 10);
+          
+          function animate() {
+              if(playerLeft === true) {
+              $("#balloon").css({"left" : "-=1px"});
+            } if (playerUp) {
+              $("#balloon").css({"top" : "-=1px"});
+            } if (playerRight) {
+              $("#balloon").css({"left" : "+=1px"});
+            } if (playerDown) {
+              $("#balloon").css({"top" : "+=1px"});
+            }
+      
+        }
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
