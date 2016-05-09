189029bc4f0c0ee05daa3477a5a2f69d925a8f0c
Daisy Tucker <daisyatucker@gmail.com>
2016-05-09 14:17:08 +0100

    scrolling along the screen

--- Floating_Balloon/index.js
+++ Floating_Balloon/index.js
@@ -54,35 +54,32 @@ function init() {
           
           function animate() {
               if(playerLeft === true) {
-              $("#balloon").css({"left" : "-=1px"});
+              $("#balloon").css({"left" : "-=2px"});
             } if (playerUp) {
-              $("#balloon").css({"top" : "-=1px"});
+              $("#balloon").css({"top" : "-=2px"});
             } if (playerRight) {
-              $("#balloon").css({"left" : "+=1px"});
+              $("#balloon").css({"left" : "+=2px"});
             } if (playerDown) {
-              $("#balloon").css({"top" : "+=1px"});
+              $("#balloon").css({"top" : "+=2px"});
             }    
         }
 
     //================BALLOON FLOATING EFFECT======================//
 
      
-        //   var balloon = $("#balloon")
-        
-        //     function runIt() {
-        //     balloon.animate({top:'+=15'}, 1000);
-        //     balloon.animate({top:'-=15'}, 1000, runt);
-        // }
+          // var balloon = $("#balloon")
 
-        // runIt();
+          // setInterval(function() {
+          //   balloon.css({ top:'+=1'}, 2000)
+          // })
 
 //===============================GET X & Y COORDS===================================//
 
-    $("body").click(function(e) {
-         var offset = $(this).offset();
-         console.log("X" + (e.pageX - offset.left));
-         console.log("Y" + (e.pageY - offset.top));
-     });
+    // $("body").click(function(e) {
+    //      var offset = $(this).offset();
+    //      console.log("X" + (e.pageX - offset.left));
+    //      console.log("Y" + (e.pageY - offset.top));
+    //  });
 //=====================COLLISION DETECTION==============//
 
 function collision(balloon, aDiv) {
@@ -104,10 +101,9 @@ function collision(balloon, aDiv) {
     }
 
 //============detection every 0.2 (200ms) seconds==================//
-///////////////////sort base and add here!!!!!!!!!!!!!!!
-var burstOptions =  [ $('#tall'), $('#wharf'), $('#pauls'), $('#pickle'), $('#pickle2'), $('#tower'), $('#eye'), $('#bridgeL'), $('#bridgeR'), $('#parliament'), $('#parliament2'), $('#parliament3'), $('#bb'), $('#shard'), $('#shard2'), $('#top'), $('#right'), $('#left'), $('.camera')];
 
 window.setInterval(function() {
+  var burstOptions =  [ $('#top'), $('#right'), $('#left'), $('#base'), $(".pop")];
       $(burstOptions).each(function(index, element) {
           if(collision($('#balloon'),  element)) {
                  console.log("hit" , element);
@@ -130,17 +126,98 @@ var player2Score = 0;
              $('.camera').remove();
            }, 5000); 
     });
-       
 
-    // function blink(time, interval){
-    //     var timer = window.setInterval(function(){
-    //         $("img").css("opacity", "0.1");
-    //         window.setTimeout(function(){
-    //             $("img").css("opacity", "1");
-    //         }, 100);
-    //     }, interval);
-    //     window.setTimeout(function(){clearInterval(timer);}, time);
-    // }
+    //=====================OBJECT OF IMAGES AND SPECIFICATIONS===============//
+var obstacleTypes = [
+{ "height": 400,
+  "width": 300,
+  "background-image": "url('http://preview.turbosquid.com/Preview/2014/07/10__11_04_03/1.jpg29a3bef4-bdc5-40d3-b98a-55ef46374923Original.jpg')"
+},
+{ "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+}, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://preview.turbosquid.com/Preview/2014/07/10__11_04_03/1.jpg29a3bef4-bdc5-40d3-b98a-55ef46374923Original.jpg')",
+  }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+    }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://preview.turbosquid.com/Preview/2014/07/10__11_04_03/1.jpg29a3bef4-bdc5-40d3-b98a-55ef46374923Original.jpg')",
+      }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+        }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+          }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+  "bottom" : 0,
+            }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+  "bottom" : 0,
+        }, {
+  "height": 300,
+  "width": 300,
+  "background-image": "url('http://www.wallquotes.com/sites/default/files/styles/uc_canvas/public/arts0018-80.png?itok=rIdlshah')",
+  "bottom" : 0,
+        }, {
+  "height": 100,
+  "width": 150,
+  "background-image": "url('http://pad2.whstatic.com/images/thumb/8/83/Draw-a-Plane-Step-9.jpg/aid1437665-728px-Draw-a-Plane-Step-9.jpg')",
+  "top" : "50px",
+                  }
+  ]; //END OF ARRAY
+
+  //==============GET A RANDOM OBJECT OUT OF THE ARRAY - pass to createObstacle ================//
+
+  function pickAnObstacle(){
+
+    var obstacleNumber = Math.floor(Math.random() * obstacleTypes.length);
+    var myObstacle = obstacleTypes[obstacleNumber];
+
+    return myObstacle;
+
+    // myObstacle.splice(); -- remove this object so it cant be picked again?!
+
+  }
+  
+//========================PUSH ONTO THE PAGE===========================//
+
+function createObstacle(obstacle){
+  
+   // for (i = 0 ; i <= obstacleTypes.length; i++) {
+    
+        var newObstacle = $("<div></div>");
+
+        newObstacle.css(obstacle);
+        newObstacle.addClass("pop");
+
+        $(".box").prepend(newObstacle);
+        newObstacle.animate({left: -obstacle.width}, 7000 , function(){
+        newObstacle.remove() });
+     // }
+
+}
+
+// createObstacle(pickAnObstacle());
 
 
+setInterval(function(){ 
+
+  createObstacle(pickAnObstacle()) }, 3000);
+
+
+
+// END OF THE PROGRAM
 }
